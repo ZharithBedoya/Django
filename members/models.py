b4c12326213d73ae19cb61c282f615ac6a2b7737
ZharithBedoya <ZharithBedoya>
2025-11-14 11:19:04 -0500

    Creacion de models.py y agregar datos a la tabla, tambien usar CRUD

--- members/models.py
+++ members/models.py
@@ -3,5 +3,8 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    document = models.IntegerField(max_length=20, null=True)
     phone = models.IntegerField(null=True)
     joined_date = models.DateField(null=True)
+    email = models.EmailField(max_length=255, null=True)
+    age = models.IntegerField(null=True)
